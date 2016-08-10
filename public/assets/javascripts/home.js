3ce14a5d0ce295ca3122385dfdcb50fda459c284
Michael Gregory <mgregory8219@gmail.com>
2016-08-10 17:25:49 -0400

    added js animations for landing screen

--- public/assets/javascripts/home.js
+++ public/assets/javascripts/home.js
@@ -1,4 +1,12 @@
 function ready() {
+  $('h1').hide();
+  $('h3').hide();
+  $('div.navbar').hide();
+  $('h1').show("fade", 1200, function() {
+    $('h3').show("fade", 1200, function() {
+      $('div.navbar').show("slide", {direction: "up", easing: "easeOutBack"}, 600)
+    })
+  });
   // alert("working");
 };
 
