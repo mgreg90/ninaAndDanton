ea5c3136bf66703823eeb9569588399dc8435046
Michael Gregory <mgregory8219@gmail.com>
2016-08-11 14:27:00 -0400

    added about me title and some js for it, moving on to ceremony page

--- public/assets/javascripts/about_us.js
+++ public/assets/javascripts/about_us.js
@@ -4,12 +4,15 @@ function ready() {
   $('div#image-carousel').hide();
   $('div#about-us-text').hide();
   $('div.vert-separator').hide();
+  $('h2').hide();
 
   // Show stuff
-  $('div.body-container').show('fade', 1500, function() {
-    $('div#image-carousel').show('fade', 1500);
-    $('div#about-us-text').show('fade', 1500);
-    $('div.vert-separator').show('fade', 1500);
+  $('h2').show('fade', 1500, function() {
+    $('div.body-container').show('fade', 1500, function() {
+      $('div#image-carousel').show('fade', 1500);
+      $('div#about-us-text').show('fade', 1500);
+      $('div.vert-separator').show('fade', 1500);
+    });
   });
 };
 
