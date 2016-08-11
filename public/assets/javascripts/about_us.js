4f780f2fade88c64fa6737ba1b9e963e4893c752
Michael Gregory <mgregory8219@gmail.com>
2016-08-11 14:04:48 -0400

    added js for about us, added bundled gems

--- public/assets/javascripts/about_us.js
+++ public/assets/javascripts/about_us.js
@@ -1,5 +1,16 @@
 function ready() {
+  // Hide stuff
+  $('div.body-container').hide();
+  $('div#image-carousel').hide();
+  $('div#about-us-text').hide();
+  $('div.vert-separator').hide();
 
+  // Show stuff
+  $('div.body-container').show('fade', 1500, function() {
+    $('div#image-carousel').show('fade', 1500);
+    $('div#about-us-text').show('fade', 1500);
+    $('div.vert-separator').show('fade', 1500);
+  });
 };
 
-$(document).on('ready', ready());
+$(document).on('ready load', ready());
