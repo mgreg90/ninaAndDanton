a8493f26ac0c6f2fa9c0e4532f7c3726f4a0cd60
Michael Gregory <mgregory8219@gmail.com>
2016-08-15 14:34:43 -0400

    fixed carousel for wedding party and added images

--- public/assets/javascripts/wedding_party.js
+++ public/assets/javascripts/wedding_party.js
@@ -1,17 +1,17 @@
 function ready() {
   // Hide stuff
   $('div.body-container').hide();
-  $('div#wedding-party-image').hide();
-  $('div#wedding-party-text').hide();
-  $('div.vert-separator').hide();
+  $('div.wedding-party-carousel-content').hide();
   $('h2').hide();
+  $('a.carousel-control').hide();
+  $('ol.carousel-indicators').hide();
 
   // Show stuff
   $('h2').show('fade', 1200, function() {
     $('div.body-container').show('fade', 1200, function() {
-      $('div#wedding-party-image').show('fade', 1200);
-      $('div#wedding-party-text').show('fade', 1200);
-      $('div.vert-separator').show('fade', 1200);
+      $('div.wedding-party-carousel-content').show('fade', 1200);
+      $('a.carousel-control').show('fade', 1200);
+      $('ol.carousel-indicators').show('fade', 1200);
     });
   });
 };
